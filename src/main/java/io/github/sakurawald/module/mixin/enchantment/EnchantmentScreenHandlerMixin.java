9f48548a8b695b1e6aadffcf5d89f3d2eef852d9
sakurawald <sakurawald@gmail.com>
2024-07-07 17:19:29 +0800

    merge: from dev

--- src/main/java/io/github/sakurawald/module/mixin/enchantment/EnchantmentScreenHandlerMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/enchantment/EnchantmentScreenHandlerMixin.java
@@ -15,6 +15,7 @@ public class EnchantmentScreenHandlerMixin {
     @ModifyArg(method = "method_17411(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;calculateRequiredExperienceLevel(Lnet/minecraft/util/math/random/Random;IILnet/minecraft/item/ItemStack;)I"), index = 2)
     int injected(int i) {
         var enchantment = Configs.configHandler.model().modules.enchantment;
+
         if (enchantment.enable) {
             EnchantmentScreenHandler instance = ((EnchantmentScreenHandler) (Object) this);
             if (instance instanceof MyEnchantmentScreenHandler) {
